8febdbe5b99a6d4de4d63e9ad1380efda6131f3f
Syaf Djamil <the.syaf@gmail.com>
2017-09-28 14:01:51 +1000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170925060453) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
